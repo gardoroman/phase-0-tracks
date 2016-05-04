12b1795c07ec07e59890b5f7b9fa1208413aa7e2
gardoroman <gardoroman@gmail.com>
2016-05-03 19:01:56 -0500

    Adding vampire-detection questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year where you born"
+birth_year = gets.chomp
+puts "Should we order garlic bread for you? (y,n)"
+likes_garlic_bread = gets.chomp.downcase
+puts "Would you like to enroll in our company insurance plan? (y,n)"
+needs_insurance = gets.chomp.downcase
+puts likes_garlic_bread
