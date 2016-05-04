f70da7b786220f79bda4aa6b1e0b931c73516973
gardoroman <gardoroman@gmail.com>
2016-05-03 21:21:47 -0500

    Adding initial conditional logic and input validation

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,11 +1,31 @@
 puts "What is your name?"
 name = gets.chomp
 puts "How old are you?"
-age = gets.chomp
-puts "What year where you born"
-birth_year = gets.chomp
+age = gets.chomp.to_i
 puts "Should we order garlic bread for you? (y,n)"
 likes_garlic_bread = gets.chomp.downcase
+until (likes_garlic_bread  == 'n') or (likes_garlic_bread == 'y')
+  puts "please enter y or n"
+  likes_garlic_bread = gets.chomp
+end
+
 puts "Would you like to enroll in our company insurance plan? (y,n)"
 needs_insurance = gets.chomp.downcase
-puts likes_garlic_bread
+until (needs_insurance  == 'n') or (needs_insurance == 'y')
+  puts "please enter y or n"
+  needs_insurance = gets.chomp
+end
+
+
+if needs_insurance == 'y'
+  needs_insurance = true
+else
+  needs_insurance = false
+end
+
+case true
+when (age < 110) && needs_insurance
+  puts "Probably not a vampire"
+else
+  puts "Probably a vampire"
+end
