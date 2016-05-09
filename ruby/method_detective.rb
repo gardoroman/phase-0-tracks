7d539766e4ae62f92c19c9cc713550a76e2a3d28
gardoroman <gardoroman@gmail.com>
2016-05-09 15:13:25 -0500

    Adding puts statements to print the solution and adding headings to clarify the exercises

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,50 +4,64 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase
+puts "Method 1: swapcase"
+p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
+puts " "
 
- "zom".insert(2,"o")
+puts "Method 2: insert"
+p "zom".insert(2,"o")
 # => “zoom”
+puts " "
 
-
- "enhance".center(15)
+puts "Method 3: center"
+p "enhance".center(15)
 # => "    enhance    "
+puts " "
 
-
-"Stop! You’re under arrest!".upcase
+puts "Method 4: upcase"
+puts "\"Stop! You’re under arrest!\"".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
+puts " "
 
-
-"the usual" + " suspects"
-"the usual".insert(9, " suspects")
-"the usual".concat(" suspects")
+puts "Method 5: insert and concat"
+p "the usual" + " suspects"
+p "the usual".insert(9, " suspects")
+p "the usual".concat(" suspects")
 #=> "the usual suspects"
+puts " "
 
-
-" suspects".insert(0, "the usual")
-" suspects".prepend("the usual")
+puts "Method 6: insert and prepend"
+p " suspects".insert(0, "the usual")
+p " suspects".prepend("the usual")
 # => "the usual suspects"
+puts " "
 
-
-"The case of the disappearing last letter"[0..38]
-"The case of the disappearing last letter".chop
+puts "Method 7: slice and chop"
+p "The case of the disappearing last letter"[0..38]
+p "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
+puts " "
 
-
-"The mystery of the missing first letter"[1..-1]
-"The mystery of the missing first letter".delete("T")
+puts "Method 8: slice and delete"
+p "The mystery of the missing first letter"[1..-1]
+p "The mystery of the missing first letter".delete("T")
 # => "he mystery of the missing first letter"
+puts " "
 
-"Elementary,    my   dear        Watson!".squeeze(" ")
+puts "Method 9: squeeze"
+p "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
+puts " "
 
- "z".ord
+puts "Method 10: ord"
+p "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 # answer: 122 is the integer that represents the character "z"
+puts " "
 
-
-"How many times does the letter 'a' appear in this string?".count("a")
+puts "Method 11: count"
+p "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
