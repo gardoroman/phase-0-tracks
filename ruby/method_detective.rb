95b361b99daf23d15d0414be2bec72375ae49502
gardoroman <gardoroman@gmail.com>
2016-05-09 13:10:49 -0500

    Updating file with several of the missing string methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -6,21 +6,29 @@
 
 # "iNvEsTiGaTiOn".<???>
 # => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
 
 # "zom".<???>
 # => “zoom”
+"zom".insert(2,"o")
 
 # "enhance".<???>
 # => "    enhance    "
+"enhance".center(15)
 
 # "Stop! You’re under arrest!".<???>
 # => "STOP! YOU’RE UNDER ARREST!"
+"Stop! Youre under arrest!".upcase
 
 # "the usual".<???>
 #=> "the usual suspects"
+"the usual" + " suspects"
+"the usual".insert(9, " suspects")
+"the usual".concat(" suspects")
 
 # " suspects".<???>
 # => "the usual suspects"
+" suspects".insert(0, "the usual")
 
 # "The case of the disappearing last letter".<???>
 # => "The case of the disappearing last lette"
@@ -34,6 +42,8 @@
 # "z".<???>
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# answer: 122 is the integer that represents the character "z"
+"z".ord
 
 # "How many times does the letter 'a' appear in this string?".<???>
 # => 4
