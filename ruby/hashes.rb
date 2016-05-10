03bdb5ac3c4b8b9251fcb675388e8d229f70913a
gardoroman <gardoroman@gmail.com>
2016-05-10 14:11:17 -0500

    Adding prompt to change values

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -3,9 +3,12 @@
 #A series of prompts will assign values to keys in a hash table.
 #Symbols will be used as keys.
 
+#-----------------------------------------
+
 #DATA SECTION
 client_array = ["name", "age", "number of children", "decor theme", "budget", "home type"]
 
+#-----------------------------------------
 
 #METHOD DECLARATIONS
 #get_client_data receives an array which is used to prompt designer for client
@@ -16,7 +19,7 @@ def get_client_data(input_array)
     
   puts "Please enter the client's details:"
   for field in input_array
-    print "#{field}:"
+    print "#{field}: "
     client_data[field.to_sym] = gets.chomp
     puts ""
   end
@@ -27,11 +30,33 @@ end
 #print_client_data takes in a hash and outputs the key and values
 def print_client_data(input_hash)
   input_hash.each do |key, val|
-    puts "#{key} is #{val} "
+    puts "#{key}: is #{val} "
   end  
 end
 
+#convert_client_data converts string input into the appropriate data type
+def convert_client_data(input_hash)
+  x = 1
+end
 
+#asks designer if changes are needed and then corrects the entries
+def update_client_data(input_hash)
+  puts "Type what item you want to change, otherwise type 'none'"
+  response = gets.chomp.downcase
+  if response != "none"
+    update_key = response.chomp.to_sym
+    puts "What is the new value?"
+    input_hash[update_key] = gets.chomp
+  end
+  puts ""
+  return input_hash
+end
+
+#-----------------------------------------
 
 #Application Section
 client_hash =  get_client_data(client_array)
+client_hash.default = "Item Not Found"
+
+print_client_data(client_hash)
+print_client_data(update_client_data(client_hash))
