73b0a1cd2dc3424a01bfdc017018b27e209a8c00
gardoroman <gardoroman@gmail.com>
2016-05-10 15:40:18 -0500

    Adding methods to create and modify arrays

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,17 @@
+def build_array(item1, item2, item3)
+  main_array = []
+  main_array << item1
+  main_array << item2
+  main_array << item3
+  return main_array
+end
+
+def add_to_array(array1, add_item)
+  array1 << add_item
+  return array1
+end
+
+
 #list of items
 grocery_list = []
 grocery_list << "Milk"
@@ -24,3 +38,9 @@ more_items = ["Cereal", "Pizza", "Orange Juice"]
 complete_list =  grocery_list + more_items
 
 p complete_list
+
+p build_array(5,6,0)
+
+p add_to_array([],"something")
+
+p add_to_array(complete_list, "Coffee")
