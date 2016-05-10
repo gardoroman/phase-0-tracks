f236e9b630e6ed190e195dec5a6fa37b4f72a5bd
gardoroman <gardoroman@gmail.com>
2016-05-10 15:30:04 -0500

    Testing new array methods

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,2 +1,26 @@
-test_array = []
-p test_array
+#list of items
+grocery_list = []
+grocery_list << "Milk"
+grocery_list << "Eggs"
+grocery_list << "Bacon"
+grocery_list << "Spinach"
+grocery_list << "Ice Cream"
+
+p grocery_list
+
+#deleting item at index 2
+p grocery_list.delete_at(2)
+
+p grocery_list
+
+p grocery_list.insert(2,"Turkey Bacon")
+
+p grocery_list.shift
+
+p grocery_list.include?("Radishes")
+
+more_items = ["Cereal", "Pizza", "Orange Juice"]
+
+complete_list =  grocery_list + more_items
+
+p complete_list
