f671036caaeaf6c077ab40f9b601b2c204f8c8f7
gardoroman <gardoroman@gmail.com>
2016-05-11 16:23:08 -0500

    Initial test of nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,16 @@
+
+miles_davis = {
+album: 'Kind of Blue',
+released: 'August 17, 1959',
+recorded: 'March 2, 1959 – April 22, 1959',
+label: 'Columbia',
+songs: [
+  'So What',
+  'Freddie Freeloader',
+  'Blue in Green',
+  'All Blues',
+  'Flamenco Sketches',
+]
+}
+
+p miles_davis
