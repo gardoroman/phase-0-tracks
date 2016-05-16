3ef24cc73fe922335f15fd9c3e3033c1f1e6267d
gardoroman <gardoroman@gmail.com>
2016-05-16 12:57:51 -0500

    Designing Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+=begin
+
+initial design for Puppy class
+Class Puppy
+
+Characteristics
+size = small
+cuddly = true
+fur_type = "unknown"
+fur_color = "unknown"
+eye_color = "unknown"
+potty_trained = false
+
+Behavior
+drink_milk 
+run 
+play
+bark
+chase cats
+=end
