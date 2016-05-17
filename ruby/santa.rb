207e0e379f83d9c43adf4a80f3274c21521fe607
gardoroman <gardoroman@gmail.com>
2016-05-17 10:22:42 -0500

    Adding new instance methods and new setter and getter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,6 +5,7 @@ class Santa
   #Initializes the Santa instance
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance"
+    puts ""
     @gender = gender
     @ethnicity = ethnicity
     @age = 0
@@ -14,14 +15,52 @@ class Santa
   #Prints a greeting from Santa instance
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
+    puts ""
   end
   
   #Takes in a string with the name of a cookie and
   #prints a message
   def eat_milk_and_cookies(cookie)
     puts "That was a good #{cookie}"
+    puts ""
   end
-end
+
+  #increments age by 1 year
+  def celebrate_birthday
+    @age += 1
+  end
+
+  #looks up reindeer argument and moves it to the end of the list
+  def get_mad_at(bad_deer)
+    reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    puts "Original Ranking of Reindeer"
+    puts reindeer_ranking
+
+    if reindeer_ranking.include?(bad_deer)
+      last_deer = reindeer_ranking.delete(bad_deer)
+      reindeer_ranking << last_deer
+      puts "New Ranking of Reindeer"
+      puts reindeer_ranking
+    end
+
+  end
+  
+  #getter methods
+  def age
+    @age    
+  end
+
+  def gender
+    @gender    
+  end
+
+  #setter methods
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+end #end of class
+
 
 #DRIVER CODE
 kris_kringle = Santa.new('male', 'Inuit')
@@ -30,7 +69,7 @@ kris_kringle.speak
 
 kris_kringle.eat_milk_and_cookies("Chocalate Chip")
 
-reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
 
 diverse_list = [
   ["female", "Inuit"],
@@ -49,4 +88,10 @@ diverse_list.each do |gender, ethnicity|
   santa << Santa.new(gender, ethnicity)
 end
 
-puts santa
+kris = Santa.new('male','irish')
+
+
+kris.celebrate_birthday
+puts kris.age
+kris.get_mad_at("Dasher")
+puts kris.gender= 'female'
