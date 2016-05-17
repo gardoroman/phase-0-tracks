73e7ebd1be5e12806ea9a6709c1e7c1dc551c998
gardoroman <gardoroman@gmail.com>
2016-05-17 11:51:47 -0500

    Formatting and reorganizing code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,5 @@
+#CLASS DECLARATIONS
+
 #the Santa class is builds an Santa instance that speaks and 
 #gives compliments on cookies received.
 class Santa
@@ -36,84 +38,86 @@ class Santa
   #looks up reindeer argument and moves it to the end of the list
   def get_mad_at(bad_deer)
     reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    puts "Original Ranking of Reindeer"
-    puts reindeer_ranking
+    puts "Original Ranking of #{bad_deer}"
+    p reindeer_ranking
 
     if reindeer_ranking.include?(bad_deer)
       last_deer = reindeer_ranking.delete(bad_deer)
       reindeer_ranking << last_deer
-      puts "New Ranking of Reindeer"
-      puts reindeer_ranking
+      puts "New Ranking of #{bad_deer}"
+      p reindeer_ranking
     end
 
   end
   
-  #getter methods
+end #end of Santa class
 
+#-------------------------------------------------------------------------------
 
-  #def gender
-  #  @gender    
+#EXTERNAL METHOD DECLARATION
 
-
-  #setter methods
-  #def gender=(new_gender)
-  #  @gender = new_gender
-  #@end
-
-end #end of class
-
-
-#DRIVER CODE
-kris_kringle = Santa.new('male', 'Inuit')
-
-kris_kringle.speak
-
-kris_kringle.eat_milk_and_cookies("Chocalate Chip")
-
-
-#automatically generates santa instances by randomly selecting from gender and ethnic arrays.
-def santa_simulator
+#automatically generates the amount of santa instances specified by num_instances.
+#instances will randomly select from the gender and ethnicity arrays
+def santa_simulator(num_instances)
+  #arrays to generate Santa instances
   santa = []
   example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "trans", "don't know"]
   example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", 
     "Mystical Creature (unicorn)", "N/A", "Scottish", "Pinoy", "Nigerian"]
   
+  #get length of arrays to determine the max random number per array
   gender_num = example_genders.length
   ethnic_num = example_ethnicities.length
-  max_num = gender_num > ethnic_num ? gender_num : ethnic_num
 
-  
-  max_num.times do   
+  #assign random variables to Santa instance from ethnicity and gender arrays
+  num_instances.times do   
     santa << Santa.new(example_genders[rand(gender_num)], example_ethnicities[rand(ethnic_num)])
   end
 
-  puts ""
-  #puts "santa before"
-  p santa
+  #assign Santa instance a random age from 0 to 140
   santa.each {|santa_instance|  santa_instance.age = rand(140) }
-#santa[santa_instance].age(rand(140))
 
   puts ""
-  puts ""
-  #puts "santa after"
-  p santa
   
+  #Printing out the attributes of every Santa instance
   santa.each do |instance| 
-    puts "This Santa's gender is #{instance.gender} and ethnicity is #{instance.ethnicity} is #{instance.age} years old "
+    puts "This Santa's gender is #{instance.gender.capitalize} and ethnicity is #{instance.ethnicity.capitalize} is #{instance.age} years old "
+    puts ""
   end
 end
-  
+
+#-------------------------------------------------------------------------------
+
+#DRIVER CODE
+
+puts "RELEASE 1"
+puts "Initial test of Santa Claus"
+kris_kringle = Santa.new('male', 'Inuit')
+kris_kringle.speak
+kris_kringle.eat_milk_and_cookies("Chocalate Chip")
+puts ""  
+
+puts "RELEASE 2 and 3"
+puts "Testing attributes and instance methods"
+mari_ijima = Santa.new('male','Japanese')
+puts "Santa's current age"
+puts mari_ijima.age
+mari_ijima.celebrate_birthday
+puts "Santa's new age"
+puts mari_ijima.age
+puts "Santa's ethnicity is #{mari_ijima.ethnicity}"
+puts "Santa is mad at Dasher"
+mari_ijima.get_mad_at("Dasher")
+puts "Santa's current gender"
+puts mari_ijima.gender
+puts "Santa's updated gender"
+puts mari_ijima.gender= 'female'
 
 
-kris = Santa.new('male','irish')
+puts ""
 
-puts kris.age
 
-kris.celebrate_birthday
-puts kris.age
-puts kris.ethnicity 
-#kris.get_mad_at("Dasher")
-puts kris.gender
-puts kris.gender= 'female'
+puts "RELEASE 4"
 
-santa_simulator
+puts "Testing Santa Simulator"
+santa_simulator(20)
