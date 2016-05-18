8ca0d04ac2a001cef1642808938657031c13bec3
gardoroman <gardoroman@gmail.com>
2016-05-18 10:20:23 -0500

    Initial design of Book object

---
+++ ruby/solo.rb
@@ -0,0 +1,24 @@
+=begin
+DESIGN
+Class Requirements
+ - at least 3 attributes (at least two data types)
+ - at least 3 methods (at least one must take an argument)
+ Release 2 Requirements
+ - allow user to input as many instances of the class
+ - instances are stored in an array
+
+Class Book
+will have the following attributes (use attr_accessor and reader)
+-Author
+-Title
+-number of pages
+-review
+
+will have these method
+-show_book_info
+-add_review
+-print_reivew
+=end
+
+
+
