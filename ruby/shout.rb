49ac5f376f01151229e81429d1949067e6485e2f
gardoroman <gardoroman@gmail.com>
2016-05-18 13:58:33 -0500

    Adding two yelling methods to module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,17 @@
+#Module Declaration
 module Shout
-  
+
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+    words + " !!" + " happy happy :) :) :)"
+  end
+
 end
+
+#Driver Code
+
+puts Shout.yell_angrily("arghhhh")
+puts Shout.yell_happily("yippy")
