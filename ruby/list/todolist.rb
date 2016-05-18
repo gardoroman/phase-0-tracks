0a2d3fa0790fe1a0b7d4b15ecb071ea7a2955618
gardoroman <gardoroman@gmail.com>
2016-05-18 18:54:58 -0500

    Adding delete and get item methods

--- ruby/list/todolist.rb
+++ ruby/list/todolist.rb
@@ -12,5 +12,13 @@ class TodoList
   def add_item(item_string)
     @items << item_string
   end
+
+  def delete_item(item_name)
+    @items.delete(item_name)
+  end
+
+  def get_item(item_index)
+    @items[item_index]
+  end
   
 end
