555221b2babf660a41641f5fe9bafa8f73ef6674
gardoroman <gardoroman@gmail.com>
2016-05-24 17:36:27 -0500

    Add basic horse and color arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,1 +1,14 @@
 //data structures
+
+var colorArr = ["Lizard Blue", "Arizona Tan", "Corn Silk", "Tame Watermelon"];
+var horseArr = ["Abaccus", "Madison Avenue", "Rain Drops on Roses", "Where's Waldo"];
+
+colorArr.push("Papaya Whip");
+horseArr.push("Zander");
+
+
+
+
+// DRIVER CODE
+console.log(colorArr);
+console.log(horseArr);
