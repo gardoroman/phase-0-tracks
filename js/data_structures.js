ec17485eaa55bbf9474efd73d921516e6b87b734
gardoroman <gardoroman@gmail.com>
2016-05-24 18:29:36 -0500

    Add Car object with function to test if car is antique

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,12 +12,32 @@ Declare new object whose key will be a horse's name and value will be a color.
 Iterate through two arrays of equal length contain horse names and colors.
 */
 var horseObj = {}
-for (i=0; i < colorArr.length; i++){
+for (var i=0; i < colorArr.length; i++){
   horseObj[colorArr[i]] = horseArr[i];
 }
 
+function Car(model, make, color, year){
+  
+  this.model=model;
+  this.make=make;
+  this.color=color;
+  this.year=year;
+  
+  // determine if the car is an antique
+  // an antique is any car that is older than 25 years
+  this.isAntique = function(){
+    var currentYear = new Date().getFullYear();
+    if(currentYear-this.year >=25){
+      return true;
+    }
+  }
+  
+}
 
-// DRIVER CODE
 
-
-console.log(horseObj);
+// DRIVER CODE for Release 0 .. 1
+// console.log(horseObj);
+ 
+// DRIVER CODE for Release 2
+var toyotaCorolla = new Car("Corolla","Toyota","Papaya Whip",1973);
+console.log("Is antique:" + toyotaCorolla.isAntique());
