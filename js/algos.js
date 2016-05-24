7c07f7446e3e2221f831f35562dc5d96ef1be488
gardoroman <gardoroman@gmail.com>
2016-05-24 10:03:48 -0500

    Adding pseudocode for longest word algorithm

---
+++ js/algos.js
@@ -0,0 +1,13 @@
+/*
+create a function that takes an array of words and
+finds the longest word.
+The array will be passed into a for loop where every
+item will be checked for length.
+The item will be compared to the length of a place-holder
+variable that initially will be set to "" and will store
+the longest word as the for loop executes.
+*/
+function findLongestWord (input_arr){
+
+
+}
