639e424d91b1caca404361180d905258ba3815e8
gardoroman <gardoroman@gmail.com>
2016-06-01 17:01:41 -0500

    Change sql statements

--- databases/session_scheduler/scheduler.rb
+++ databases/session_scheduler/scheduler.rb
@@ -4,11 +4,13 @@ require 'time'
 require 'sqlite3'
 require 'faker'
 
+user_dbname = "user.db"
+user_session_dbname = "user_session.db"
 # create SQLite3 database
-user_db = SQLite3::Database.new("user.db")
+user_db = SQLite3::Database.new(user_db_name)
 user_db.results_as_hash = true
 
-session = SQLite3::Database.new("session.db")
+session_db = SQLite3::Database.new(user_session_dbname)
 session_db.results_as_hash = true
 
 # learn about fancy string delimiters
@@ -19,24 +21,23 @@ create_user_db = <<-SQL
     last_name VARCHAR(255)
   )
 SQL
-db.execute(create_user_db)
+user_db.execute(create_user_db)
 
 create_session_db = <<-SQL
-  CREATE TABLE IF NOT EXISTS user(
+  CREATE TABLE IF NOT EXISTS user_session(
     session_id INTEGER PRIMARY KEY,
     user_id INT,
-    FOREIGN KEY (user_id) REFERENCES user(user_id),
     assignment VARCHAR(255),
     session_date DATE,
     day_of_week VARCHAR(10),
+    session_tz VARCHAR(255),
     earliest_ts DATETIME,
     latest_ts DATETIME,
     availability DATETIME,
-    partner_id INT,
-    status VARCHAR(255)
+    partner_id INT DEFAULT NULL,
+    status VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
   )
 SQL
-db.execute(create_session_db)
+session_db.execute(create_session_db)
 
-# session\_id (primary key), user\_id (foreign key), assignment, date, day\_of\_week, time\_zone,
-#earliest, latest, availability, partner\_id, status
