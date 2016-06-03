1681782923b10102fed8cc6ec11f99ce5fe7c62f
gardoroman <gardoroman@gmail.com>
2016-06-03 09:32:12 -0500

    Include user names when displaying matches. Standardize naming on tables.

--- databases/session_scheduler/matcher.rb
+++ databases/session_scheduler/matcher.rb
@@ -1,10 +1,15 @@
 =begin
 matcher.rb
-1. retrieves all records for a single user from the user_session table
-2. searches for other users in user_session that have the same date, session_number
-3. matches records where there is an overlap of at least 2 hours
+1. retrieves all records for a single user from the user_schedule table
+2. searches for other users in user_schedule that have the same date, session_number
+3. matches records where there is an overlap of at least 1 hour
+
+Note: 
+The fields earliest and latest are integers that represent 1 hour in a 24 hour period in UTC.
+Later versions will convert the hours to the user's local time.
 =end
 require 'sqlite3'
+require 'time'
 
 
 db = SQLite3::Database.new("schedule.db")
@@ -14,42 +19,58 @@ db.results_as_hash = true
 
 def find_match(db, search_date, user)
   query_string = 
-  ("select u1.session_date, u1.sess_num, 
-    u1.user_id id1, u1.session_tz tz1, u1.earliest early1, u1.latest latest1, 
-    u2.user_id id2, u2.session_tz tz2, u2.earliest early2, u2.latest latest2
-    from user_session u1,
-         user_session u2
-    where u1.session_date = u2.session_date
-       and u1.session_date = #{search_date}
-       and u1.sess_num = u2.sess_num
-       and u1.user_id = #{user}
-       and u2.user_id <> u1.user_id
-       and u1.partner_id is NULL
-       and u2.partner_id is NULL"
+  ("select s1.session_date, s1.session_id, 
+    u1.first_name || ' ' || u1.last_name name1, s1.session_tz tz1, s1.earliest early1, s1.latest latest1, 
+    u2.first_name || ' ' || u2.last_name name2, s2.session_tz tz2, s2.earliest early2, s2.latest latest2
+    from user_schedule s1,
+         user_schedule s2,
+         users u1,
+         users u2
+    where s1.session_date = s2.session_date
+       and s1.session_date = #{search_date}
+       and s1.session_id = s2.session_id
+       and s1.user_id = #{user}
+       and s2.user_id <> s1.user_id
+       and s1.user_id = u1.user_id
+       and s2.user_id = u2.user_id
+       and s1.partner_id is NULL
+       and s2.partner_id is NULL"
     )
   match_recs = db.execute(query_string)  
 end
 
+def convert_to_display_time(time_int)
+  if time_int > 9
+    return "#{time_int.to_s}:00"
+  else
+    return "0#{time_int.to_s}:00"
+  end
+end
+
+#calculate_overlap looks for a minimum of 1 hour 
 def calculate_overlap(early1, early2, latest1, latest2)
+  hours = Time.now
   max_early =  early1 > early2 ? early1 : early2
   min_latest = latest1 < latest2 ? latest1 : latest2
+  if (min_latest - max_early) >= 1
+    from_time = convert_to_display_time(max_early)
+    to_time = convert_to_display_time(min_latest)
+    return from_time, to_time
+  else
+    return nil
+  end
 end
 
-session_date = "'2016-06-09'"
+session_date = "'2016-04-29'"
 
-user_number = 13
+user_number = 5
 
-matches = find_match(db, session_date,user_number)
+matches = find_match(db, session_date, user_number)
 matches.each do |row|
-  puts "early 1 #{row["early1"]} early 2 #{row["early2"]}"
+  #puts "early 1 #{row["early1"].class} early 2 #{row["early2"]}"
+  overlap = calculate_overlap(row["early1"],row["early2"],row["latest1"],row["latest2"])
+  if overlap
+    puts "#{row['name1']} can work on assignment #{row['session_id']} with #{row['name2']} on #{row['session_date']} from #{overlap[0]} to #{overlap[1]} UTC"
+  end
 end
 
-#session_recs = db.execute("select * from user_session where user_id = #{user_number} and partner_id is NULL")
-
-#p session_recs
-#session_recs.each do |row|
-#  row.each {|col| puts "#{col} is a(n) #{col.class}"} 
-#end
-
-#session_recs.each {|row| puts row}
-#uts session_recs[0][1]
--- databases/session_scheduler/scheduler.rb
+++ databases/session_scheduler/scheduler.rb
@@ -1,4 +1,4 @@
-#Refer to README.md in project folder for logic and business rules.
+#scheduler.rb creates test data to load into the user_schedule table.
 
 require 'time'
 require 'sqlite3'
@@ -23,8 +23,8 @@ utc_offset = {"EST" => 5,"CST" => 6,"MST" => 7,"PST" => 8,"AKST" => 9,"HAST" => 
 
 user_insert_string = "INSERT INTO users (first_name, last_name, user_tz) VALUES (?, ?, ?)"
 session_insert_string = 
-"INSERT INTO user_session 
-(user_id, sess_num, session_date, session_tz, earliest, latest)
+"INSERT INTO user_schedule 
+(user_id, session_id, session_date, session_tz, earliest, latest)
 VALUES (?, ?, ?, ?, ?, ?)"
 
 
@@ -44,20 +44,21 @@ SQL
 db.execute(create_user_db)
 
 create_session_db = <<-SQL
-  CREATE TABLE IF NOT EXISTS user_session(
+  CREATE TABLE IF NOT EXISTS user_schedule(
     user_id INT,
-    sess_num VARCHAR(255),
+    session_id VARCHAR(255),
     session_date TEXT,
     session_tz VARCHAR(255),
     earliest INT,
     latest INT,
     partner_id INT DEFAULT NULL,
     status VARCHAR(255) DEFAULT "Active",
-    PRIMARY KEY (user_id, sess_num, session_date, partner_id),
+    PRIMARY KEY (user_id, session_id, session_date, partner_id),
     FOREIGN KEY (user_id) REFERENCES user(user_id)
   )
 SQL
 db.execute(create_session_db)
+
 
 #To add days or weeks to a Time object, the seconds
 #must be calculated and added back. A day in seconds
@@ -77,35 +78,41 @@ def insert_into_table(db, query_string, *args)
   db.execute(query_string, args)
 end
 
-=begin
-#UNCOMMENT TO LOAD USERS
-#loads users table
-20.times do
-  insert_into_table(db, user_insert_string, Faker::Name.first_name, Faker::Name.last_name, stan_tz.keys[rand(stan_tz.length)]) 
+
+
+
+#loads users table with test user data
+25.times do
+  insert_into_table(db, user_insert_string, Faker::Name.first_name, Faker::Name.last_name, utc_offset.keys[rand(utc_offset.length)]) 
 end
-=end
+
 
 #This block of code creates randomize session data. Every test user will create 3 sessions per week per assignment.
 
 user_recs = db.execute("select * from users")
 comp_week = ""
 pref_date = Time.utc(2016,4,11)
+
+
 session_number.each do |sess|
   temp_id = ""
   user_recs.each do |id|
     sess_date = ""
+
+    # create 3 records per user record per assignment
     3.times do
       if temp_id != id['user_id']
         sess_date = pref_date        
         temp_id = id['user_id']
       end
       
-      #increments dates to ensure that the same user 
+      #increments dates to ensure that the session dates are different
       sess_date = sess_date + add_days(0,rand(2) + 1)
       
-      #check dst
+      #check for daylight savings
       utc_adjust = sess_date.dst? ? utc_offset[id['user_tz']] - 1 : utc_offset[id['user_tz']]
-      #The early and latest values which represent the beginning and end of possible session times are randomized as well.
+
+      #The early and latest values represents the availability of the user
       #utc_adjust adds the hours back to get to utc.
       early = rand(10) + utc_adjust
       late = early + 2 + rand(4)
@@ -113,9 +120,11 @@ session_number.each do |sess|
       insert_into_table(db,session_insert_string,id['user_id'],sess,load_date,id['user_tz'],early,late)
     end
   end
+  #the first number of the session contains the week number. If the week number has changed 
+  #increment by 1 week.
   sess_week = sess.split(".")[0]
   if sess_week != comp_week
-    pref_date = pref_date + add_days(1,0)
+    pref_date = pref_date + add_days(1,0) #add 7 days
     comp_week = sess_week
   end
 end
