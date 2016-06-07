85bcf9fdf9b9d2b0eb13f3da8f34a37bc5d6ec1c
gardoroman <gardoroman@gmail.com>
2016-06-07 10:50:01 -0500

    Add functionality to change color of element

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -6,3 +6,14 @@ firstPara.style.margin = "25px";
 firstPara.style.padding = "25px";
 firstPara.style.height = "300px";
 firstPara.style.width = "300px";
+
+
+function changeColor(event) {
+  var redVal = document.getElementById("redVal").value;
+  var blueVal = document.getElementById("blueVal").value;
+  var greenVal = document.getElementById("greenVal").value;
+  firstPara.style.backgroundColor = "rgb(" + redVal + ", " + blueVal + ", " + greenVal + ")";
+}
+
+var colorButton = document.getElementById("changeColor");
+colorButton.addEventListener("click", changeColor);
