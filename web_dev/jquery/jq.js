ec4f0b64b5f2260defcb99aca4a2148f8e0eba5e
gardoroman <gardoroman@gmail.com>
2016-06-07 14:48:49 -0500

    Add animation to paragraphs in demo html. Set display to none on paragraph to make slideDown function work

--- web_dev/jquery/jq.js
+++ web_dev/jquery/jq.js
@@ -1,6 +1,13 @@
 $(function() {
 
-  
+  $('#para-hide').hide(2000);
 
+  $('#para-slide-up').slideUp(2000);
+      
+  $('#para-slide-down').slideDown(2000);
 
+  $('#para-hover').hover(function(){
+    $(this).fadeOut(1000);
+    $(this).fadeIn(500);
+  })
 });
