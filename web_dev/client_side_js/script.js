f096c537810838a8c34e497f8d42d5b8a2c570c0
gardoroman <gardoroman@gmail.com>
2016-06-07 09:46:51 -0500

    Add more description to html, create script to style elements.

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,8 @@
+var paras = document.getElementsByTagName("p");
+var firstPara = paras[0];
+firstPara.style.backgroundColor = "gray";
+firstPara.style.color = "white";
+firstPara.style.margin = "25px";
+firstPara.style.padding = "25px";
+firstPara.style.height = "300px";
+firstPara.style.width = "300px";
