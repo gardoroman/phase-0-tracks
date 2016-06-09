2d9b6a8ac04aa54452bd8b38227e2c8507c8e1f3
gardoroman <gardoroman@gmail.com>
2016-06-09 12:24:47 -0500

    Add form to add campus. Revise home page to give access to info. Add campus table to info page

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -11,6 +11,7 @@ db.results_as_hash = true
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
+  #@campuses = db.execute("SELECT * FROM campus")
   erb :home
 end
 
@@ -18,8 +19,13 @@ get '/students/new' do
   erb :new_student
 end
 
-get '/campus' do
+get '/info' do
   @campus_info = db.execute("select campus, round(avg(age),1) avg_age, count(*) counts from students group by campus")
+  @campuses = db.execute("select * from campus")
+  erb :info
+end
+
+get '/campus/new' do
   erb :campus
 end
 
@@ -27,6 +33,12 @@ end
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+  redirect '/'
+end
+
+#insert new campus
+post '/campus' do
+  db.execute("INSERT INTO campus (campus_code, campus_name) VALUES (?,?)", [params['campus_code'], params['campus_name']])
   redirect '/'
 end
 
